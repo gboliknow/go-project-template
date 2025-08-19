af7479a2b5fbd6bdee8456496732caaafd677551
gbolahan <adekoyegbolahan001@gmail.com>
2025-08-19 13:55:52 +0100

    :art: Added config and db structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,44 @@
 package config
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// )
+
+// type Config struct {
+// 	Port       string
+// 	DBUser     string
+// 	DBPassword string
+// 	DBAddress  string
+// 	DBName     string
+// 	JWTSecret  string
+// }
+
+// var Envs = InitializeConfig()
+
+// func InitializeConfig() Config {
+
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		fmt.Println("Error loading .env file")
+// 		fmt.Println("Error:", err)
+// 	}
+
+// 	return Config{
+// 		Port:       getEnv("PORT", "5432"),
+// 		DBUser:     getEnv("DB_USER", "user3"),
+// 		DBPassword: getEnv("DB_PASSWORD", "pass3"),
+// 		DBName:     getEnv("DB_NAME", "bildwerk"),
+// 		JWTSecret:  getEnv("JWT_SECRET", ""),
+// 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "5432")),
+// 	}
+// }
+
+// func getEnv(key, fallback string) string {
+// 	if value, ok := os.LookupEnv(key); ok {
+// 		return value
+// 	}
+// 	return fallback
+// }
---
+++ internal/database/db.go
@@ -0,0 +1,37 @@
+ package database
+
+// import (
+// 	"github.com/gboliknow/bildwerk/internal/models"
+// 	"github.com/rs/zerolog/log"
+
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// )
+
+// type PostgresStorage struct {
+// 	db *gorm.DB
+// }
+
+// func NewPostgresStorage(connStr string) (*PostgresStorage, error) {
+// 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+// 	if err != nil {
+// 		log.Error().Err(err).Msg("Unable to connect to database")
+// 	}
+
+// 	log.Info().Msg("Connected to PostgreSQL!")
+// 	return &PostgresStorage{db: db}, nil
+// }
+
+// func (s *PostgresStorage) InitializeDatabase() (*gorm.DB, error) {
+
+// 	if err := s.db.AutoMigrate(
+// 		&models.User{},
+// 		&models.Image{},
+// 	); err != nil {
+// 		log.Error().Err(err).Msg("Failed to migrate database schema")
+// 		return nil, err
+// 	}
+
+// 	log.Info().Msg("Database schema migrated successfully")
+// 	return s.db, nil
+// }
